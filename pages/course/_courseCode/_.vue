be09e83bf8bca850feb672f1069f7cd405b4f137
Iago Rodrigues <yagomello16@gmail.com>
2020-08-08 16:45:25 -0300

    ✨ feat: add unknown dynamic route (catch all) with validate method

---
+++ pages/course/_courseCode/_.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>unknown dynamic route inside course/{{ this.$route.params.courseCode }}</div>
+</template>
+
+<script>
+export default {
+  validate({ params }) {
+    const [, lessonCode] = params.pathMatch.split('/');
+    return params.courseCode && /^\d+$/g.test(lessonCode);
+  },
+};
+</script>
---
+++ pages/course/_courseCode/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    CourseCode page
+    <nuxt-link to="/course/1/lesson/1">to first lesson</nuxt-link>
+  </div>
+</template>
