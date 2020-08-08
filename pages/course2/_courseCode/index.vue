1996a0fd35485a7c0afa0feff953e8a3bbb2970f
Iago Rodrigues <yagomello16@gmail.com>
2020-08-08 16:45:57 -0300

    ✨ feat: add normal routes with query and params

---
+++ pages/course2/_courseCode/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    CourseCode page
+    <nuxt-link to="/course/1/lesson?lessonCode=1">to first lesson</nuxt-link>
+  </div>
+</template>
---
+++ pages/course2/_courseCode/lesson.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>lesson route inside course/{{ this.$route.params.courseCode }}</div>
+</template>
+
+<script>
+export default {
+  validate({ params, query }) {
+    return params.courseCode && query.lessonCode;
+  },
+};
+</script>
